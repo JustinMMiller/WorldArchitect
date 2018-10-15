430ab4ea40dcb85940e16f73c1b4d89a181291c8
Justin Miller <justintime145@gmail.com>
2018-10-15 00:10:10 -0400

    Refactored drawing a vector to account for perfectly vertical lines

--- test.c
+++ test.c
@@ -2,24 +2,41 @@
 #include<math.h>
 #include "./qdbmp.h"
 #include "./vector.h"
-#define P_WIDTH 100
-#define P_HEIGHT 100
+#define P_WIDTH 512
+#define P_HEIGHT 512
 
 void drawPointsVector(Vector v, BMP *bmp)
 {
-	int i, y;
-	float m, b;
-	m = v.direction.y / v.direction.x;
-	b = -m*v.point.x + v.point.y;
-	for(int i = v.point.x; i < (v.point.x + v.direction.x); i++)
+	float x, y, z, dx, dy, len;
+	len = hypotf(v.direction.x, v.direction.y);
+	dx = v.direction.x / len;
+	dy = v.direction.y / len;
+	x = v.point.x;
+	y = v.point.y;
+	for(z = 0; z <= len; z++)
 	{
-		y = (int)(lroundf(m*i + b));
-		printf("Setting %i, %i\n", i, y);
-		BMP_SetPixelRGB(bmp, i, y, 255, 255, 255);
+		BMP_SetPixelRGB(bmp, floorf(x), y, 255, 255, 255);
 		if(BMP_GetError() != BMP_OK)
 		{
 			printf( "An error has occurred: %s (code %d)\n", BMP_GetErrorDescription(), BMP_GetError() );
 		}
+		BMP_SetPixelRGB(bmp, ceilf(x), y, 255, 255, 255);
+		if(BMP_GetError() != BMP_OK)
+		{
+			printf( "An error has occurred: %s (code %d)\n", BMP_GetErrorDescription(), BMP_GetError() );
+		}
+		BMP_SetPixelRGB(bmp, x, floorf(y), 255, 255, 255);
+		if(BMP_GetError() != BMP_OK)
+		{
+			printf( "An error has occurred: %s (code %d)\n", BMP_GetErrorDescription(), BMP_GetError() );
+		}
+		BMP_SetPixelRGB(bmp, x, ceilf(y), 255, 255, 255);
+		if(BMP_GetError() != BMP_OK)
+		{
+			printf( "An error has occurred: %s (code %d)\n", BMP_GetErrorDescription(), BMP_GetError() );
+		}
+		x += dx;
+		y += dy;
 	}
 }
 
@@ -31,7 +48,7 @@ int main()
 	UCHAR r, g, b;
 	v.point.x = 40;
 	v.point.y = 60;
-	v.direction.x = 20;
+	v.direction.x = 0;
 	v.direction.y = 10;
 	bmp = BMP_Create(P_WIDTH, P_HEIGHT, 24);
 	if(BMP_GetError() != BMP_OK)
