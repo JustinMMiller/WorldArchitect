b05627f650e4180f818da798df060850038c9c5f
Justin Miller <justintime145@gmail.com>
2019-05-01 16:05:28 -0400

    Working on fleshing out SQLEmbeddable interface

--- src/SQLite/SQLEmbeddable.h
+++ src/SQLite/SQLEmbeddable.h
@@ -10,7 +10,9 @@ namespace WorldArchitect
 	{
 		public:
 			virtual std::string getTableName() = 0;
-			virtual std::vector<std::string> > getFieldTypes() = 0;
+			virtual std::string getColumnNames() = 0;
+			virtual std::string getSQLValueString() = 0;
+			virtual std::string getPrimaryKey() = 0;
 	};
 }
 
